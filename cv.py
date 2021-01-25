2f5683e7ad989736984810718b8fea9c20c299ec
yyw <3azyyw@gmail.com>
2021-01-25 17:02:01 +0800

    完成版_可以自訂儲存大小

--- cv.py
+++ cv.py
@@ -1,14 +1,26 @@
 import cv2 # activate tf2->pip install opencv-python
 import datetime
+import os
 # 選擇第二隻攝影機->cv2.VideoCapture(1)
 # 筆電鏡頭->cv2.VideoCapture(0)
 cap = cv2.VideoCapture(0)
 
 c = 0
 p = 0
+# 讓客戶可以給定記憶體大小，計算最多可以存幾張照片
+req = input('Please enter desired memory(unit:MB) : ')
+KB = int(req) * 1024
+quantity = KB / 9   # 因一張.jpg的大小大約9KB
+
 while(True):
   # 從攝影機擷取一張影像
   ret, frame = cap.read()
+
+  # 在圖片上加時間
+  now = str(datetime.datetime.now())
+  frame = cv2.putText(frame, now, (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                           (255, 0, 0), 2, cv2.LINE_AA)
+
   width = 160
   height = 120
   dim = (width, height)
@@ -21,14 +33,17 @@ while(True):
   # cv2.imshow('frame', frame)
   # resize image
   cv2.imshow('Resized Image', resized)
+  
 
   c = c + 1
   if c % 30 == 0:
     p = p + 1
-    x = datetime.datetime.now()
-    # cv2.imwrite(str(p)+'.jpg', frame)
-    cv2.imwrite(str(x.year) + '-' + str(x.month) + '-' + str(x.day) + ' ' + str(x.hour) + '-' + str(x.minute) + '-' + str(x.second) + '.jpg', resized)
-    if p >= 60:
+    # x = datetime.datetime.now()
+    os.makedirs('data', exist_ok=True)
+    cv2.imwrite('data/' + str(p) + '.jpg', resized)
+    # cv2.imwrite(str(p) + '.png', resized)
+    # cv2.imwrite(str(x.year) + '-' + str(x.month) + '-' + str(x.day) + ' ' + str(x.hour) + '-' + str(x.minute) + '-' + str(x.second) + '.jpg', resized)
+    if p >= quantity:
       p = 0
 
 
